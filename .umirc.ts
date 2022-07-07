c1a1e523a1432b12bde13bb23c03ac955ed79be2
kongdong <kongdong@edocyun.com.cn>
2022-07-07 13:40:41 +0800

    feat: 初始化项目

---
+++ .umirc.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    {
+      exact: false,
+      path: '/',
+      component: '@/layouts/index',
+      routes: [{ path: '/', component: '@/pages/Home' }],
+    },
+  ],
+
+  fastRefresh: {},
+});
